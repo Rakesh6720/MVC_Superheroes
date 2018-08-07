c534838d51d8b65e551180a374721bd5c45eb08e
Rakesh <rbaruah@gmail.com>
2018-08-07 14:10:11 -0500

    initial commit

---
+++ Superhero2/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+
+namespace Superhero2.Models
+{
+    public class Superhero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryPower { get; set; }
+        public string SecondaryPower { get; set; }
+        public string Catchphrase { get; set; }
+    }
+
+    public class SuperheroDbContext : DbContext
+    {
+        public DbSet<Superhero>Superheroes { get; set; }
+    }
+}
---
+++ Superhero2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Superhero2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Superhero2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd92b6d3-7bc7-4b07-bea6-0621fb01cfec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
